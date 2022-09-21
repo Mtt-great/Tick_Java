bc1ee8fc56963aa53f1e858c023ae2b26963663a
matao <810106495@qq.com>
2022-09-21 11:43:30 +0800

    9-21

--- getTickInfo/src/test/java/test.java
+++ getTickInfo/src/test/java/test.java
@@ -2,16 +2,25 @@ import com.matao.GetTickInfo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
-        GetTickInfo getTickInfo= (GetTickInfo) context.getBean("getTickInfo");
-        String[] tickInfoArray=getTickInfo.getTickInfoByTencent();
+        while ( true ) {
+            ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+            GetTickInfo getTickInfo = (GetTickInfo) context.getBean("getTickInfo");
+            String[] tickInfoArray = getTickInfo.getTickInfoByTencent();
 
-        for ( String info : tickInfoArray){
-            System.out.println( info );
+            System.out.println(tickInfoArray[1]+"---" + tickInfoArray[3] + "---" + tickInfoArray[6]);
+            if (Integer.parseInt(tickInfoArray[7]) > Integer.parseInt(tickInfoArray[8])) {
+                System.out.println("买大于卖--->买");
+            } else {
+                System.out.println("卖大于买--->卖");
+            }
+            Thread.sleep(1000);
         }
     }
 }
